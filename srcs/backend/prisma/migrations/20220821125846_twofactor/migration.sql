6006b808f8332a1da37cdf6eec1f7c323de64f22
Ermand Haruni <eharuni@k1m43s05.42istanbul.com.tr>
2022-09-07 20:47:40 +0300

    bad push

--- srcs/backend/prisma/migrations/20220821125846_twofactor/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `two_factor_enabled` to the `User` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "two_factor_enabled" BOOLEAN NOT NULL,
-ADD COLUMN     "two_factor_secret" TEXT;
--- srcs/backend/prisma/migrations/20220822114351_friendupdate/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `status` on the `Friend` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Friend" DROP COLUMN "status";
